b79f1e0e08f6f61cee56f6193463d4e529ffbce1
vikash kumar <vikashkumar24041997@gmail.com>
2025-10-03 23:46:44 +0530

    testing api

---
+++ a/abc.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 3, 4, 5];
+const reversed = [...arr].reverse(); // arr stays same
+
+console.log("Original:", arr); // [1, 2, 3, 4, 5]
+console.log("Reversed:", reversed); // [5, 4, 3, 2, 1]
